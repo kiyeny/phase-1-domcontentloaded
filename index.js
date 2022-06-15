8c17cc02dd31d3330b1430de9485a547a711dcc4
maxwell <maxwell.kiyeny@student.moringaschool.com>
2022-06-15 19:29:30 +0300

    my index.js commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", ()=>{
+    console.log("This is a very nice example!")
+    document.querySelector('p').textContent = 'This is really cool!'
+}) 
